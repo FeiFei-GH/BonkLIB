2905e3b309b15856f4f3c63b5d7220057a37e27e
Clarifie <friendlyneighborhoodemailbot1@gmail.com>
2024-05-15 08:31:15 -0400

    temporary build in no ads

--- src/BonkHUD/loadInitialization.js
+++ src/BonkHUD/loadInitialization.js
@@ -4,3 +4,9 @@ bonkHUD.loadStyleSettings();
 bonkHUD.updateStyleSettings();
 
 bonkHUD.initialize();
+
+//!implement later on a toggle to show or hide ads
+let ad1 = window.top.document.getElementById('adboxverticalCurse');
+let ad2 = window.top.document.getElementById('adboxverticalleftCurse');
+ad1.style.display = "none";
+ad2.style.display = "none";
