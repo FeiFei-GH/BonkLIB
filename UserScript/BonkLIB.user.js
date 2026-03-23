240af8018ccefec24d5a615e39bfc1a8a417c90c
FeiFei <44251254+FeiFei-GH@users.noreply.github.com>
2026-03-23 11:05:25 -0400

    bump version to 1.1.5 in user script, package.json, and package-lock.json

--- UserScript/BonkLIB.user.js
+++ UserScript/BonkLIB.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BonkLIB
-// @version      1.1.4
+// @version      1.1.5
 // @author       FeiFei + Clarifi + BoZhi
 // @namespace    https://github.com/FeiFei-GH/BonkLIB
 // @description  BonkAPI + BonkHUD
@@ -16,7 +16,7 @@ https://greasyfork.org/en/scripts/433861-code-injector-bonk-io
 
 // ! Compitable with Bonk Version 49
 window.bonkLIB = {};
-bonkLIB.version = "1.1.4";
+bonkLIB.version = "1.1.5";
 
 
 window.bonkAPI = {};
