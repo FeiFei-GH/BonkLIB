8b715adaf5cd04912808bac41c126fbbb6cb128a
FeiFei <44251254+FeiFei-GH@users.noreply.github.com>
2026-03-27 10:52:04 -0400

    bump to 1.1.6

--- UserScript/BonkLIB.user.js
+++ UserScript/BonkLIB.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BonkLIB
-// @version      1.1.5
+// @version      1.1.6
 // @author       FeiFei + Clarifi + BoZhi
 // @namespace    https://github.com/FeiFei-GH/BonkLIB
 // @description  BonkAPI + BonkHUD
@@ -16,7 +16,7 @@ https://greasyfork.org/en/scripts/433861-code-injector-bonk-io
 
 // ! Compitable with Bonk Version 49
 window.bonkLIB = {};
-bonkLIB.version = "1.1.5";
+bonkLIB.version = "1.1.6";
 
 
 window.bonkAPI = {};
