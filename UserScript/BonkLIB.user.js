de4b6d4623ba62a1e7e9bf651f600d4da08fdcb8
FeiFei <44251254+FeiFei-GH@users.noreply.github.com>
2026-03-27 10:59:23 -0400

    Fix mod crashes and update version to 1.1.6 (#19)
    * Mods' crash fix (#16)
    
    * update decodeMap & encodeMap
    
    * mapConverter fix
    
    * removed old encoder
    
    * mod crash fix
    
    * bump version to 1.1.5 in user script, package.json, and package-lock.json
    
    * mapdata fix (#18)
    
    * bump to 1.1.6
    
    ---------
    
    Co-authored-by: Apex <116424660+aApeex@users.noreply.github.com>

--- UserScript/BonkLIB.user.js
+++ UserScript/BonkLIB.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BonkLIB
-// @version      1.1.5
+// @version      1.1.6
 // @author       FeiFei + Clarifi + BoZhi
 // @namespace    https://github.com/FeiFei-GH/BonkLIB
 // @description  BonkAPI + BonkHUD
@@ -16,7 +16,7 @@ https://greasyfork.org/en/scripts/433861-code-injector-bonk-io
 
 // ! Compitable with Bonk Version 49
 window.bonkLIB = {};
-bonkLIB.version = "1.1.5";
+bonkLIB.version = "1.1.6";
 
 
 window.bonkAPI = {};
@@ -3399,34 +3399,32 @@ bonkAPI.encodeMap = function (map) {
     return compressed;
 };
 
-bonkAPI.blankMap = {
-    v: 1,
-    s: { re: false, nc: false, pq: 1, gd: 25, fl: false },
-    physics: { shapes: [], fixtures: [], bodies: [], bro: [], joints: [], ppm: 12 },
-    spawns: [],
-    capZones: [],
-    m: {
-        a: "noauthor",
-        n: "noname",
-        dbv: 2,
-        dbid: -1,
-        authid: -1,
-        date: "",
-        rxid: 0,
-        rxn: "",
-        rxa: "",
-        rxdb: 1,
-        cr: [],
-        pub: false,
-        mo: "",
-    },
-};
-
 bonkAPI.decodeMap = function (map) {
     b64mapdata = LZString.decompressFromEncodedURIComponent(map);
     binaryReader = new bonkAPI_bytebuffer();
     binaryReader.fromBase64(b64mapdata, false);
-    map = bonkAPI.blankMap;
+    map = {
+        v: 1,
+        s: { re: false, nc: false, pq: 1, gd: 25, fl: false },
+        physics: { shapes: [], fixtures: [], bodies: [], bro: [], joints: [], ppm: 12 },
+        spawns: [],
+        capZones: [],
+        m: {
+            a: "noauthor",
+            n: "noname",
+            dbv: 2,
+            dbid: -1,
+            authid: -1,
+            date: "",
+            rxid: 0,
+            rxn: "",
+            rxa: "",
+            rxdb: 1,
+            cr: [],
+            pub: false,
+            mo: "",
+        },
+    };
     map.v = binaryReader.readShort();
     if (map.v > 15) {
         throw new Error("Future map version, please refresh page");
